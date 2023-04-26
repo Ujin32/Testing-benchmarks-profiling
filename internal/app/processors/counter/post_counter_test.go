b466b7a74ccff84bbfe3147df1ad9dff6d32b692
Eugenes <smtrkw@mail.ru>
2023-04-26 16:41:28 +0300

    added tests

--- internal/app/processors/counter/post_counter_test.go
+++ internal/app/processors/counter/post_counter_test.go
@@ -5,6 +5,8 @@ import (
 
 	mock_post "module06/test/gomock/mocks"
 
+	"module06/internal/app/services/post"
+
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
@@ -17,6 +19,30 @@ func TestGetList(t *testing.T) {
 
 	mockClient := mock_post.NewMockClient(mockController)
 
-	mockClient.EXPECT().GetList().Return().Times(1)
+	t.Run("get post return empty list", func(t *testing.T) {
+		empty_list := []post.Post{}
+		mockClient.EXPECT().GetList().Return(empty_list, nil).Times(1)
+		posts, err := mockClient.GetList()
+		req.NoError(err)
+		req.Empty(posts)
+
+	})
+	t.Run("count post with empty list", func(t *testing.T) {
+		expectedPosts := []post.Post{
+			{ID: 1, Title: "Пост 1"},
+			{ID: 2, Title: "Пост 2"},
+			{ID: 3, Title: "Пост 3"},
+		}
+		mockClient.EXPECT().GetList().Return(expectedPosts, nil).AnyTimes()
+		posts, err := mockClient.GetList()
+		req.NoError(err)
+		req.NotEmpty(posts)
+
+		lenPosts, err := PostCount(mockClient)
+		req.NoError(err)
+		expPosts := len(expectedPosts)
+		req.Equal(expPosts, lenPosts, "Количество постов должно быть равным %d, а результат функции %d", expPosts, lenPosts)
+
+	})
 
 }
