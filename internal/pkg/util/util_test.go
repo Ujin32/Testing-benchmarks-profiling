8ba9682d91ae9044959c450b385722a91db61007
Eugenes <smtrkw@mail.ru>
2023-04-27 15:26:54 +0300

    added TestIsPalindrom

--- internal/pkg/util/util_test.go
+++ internal/pkg/util/util_test.go
@@ -28,11 +28,11 @@ func TestContainsDuplicate(t *testing.T) {
 		slice []int
 		want  bool
 	}{
-		"1": {[]int{1, 2, 1, 3}, true},
-		"2": {[]int{112222, 2, 44, 55}, false},
-		"3": {[]int{}, false},
-		"4": {[]int{2}, false},
-		"5": {[]int{0, 2, -1, 1}, false},
+		"Is duplicate ":           {[]int{1, 2, 1, 3}, true},
+		"Is not duplicate ":       {[]int{112222, 2, 44, 55}, false},
+		"Empty slice":             {[]int{}, false},
+		"One value in slice":      {[]int{2}, false},
+		"Negative value in slice": {[]int{0, 2, -1, 1}, false},
 	}
 
 	for name, testCase := range cases {
@@ -41,5 +41,22 @@ func TestContainsDuplicate(t *testing.T) {
 			require.Equal(t, testCase.want, res)
 		})
 	}
+
+}
+
+func TestIsPalindrom(t *testing.T) {
+	palidromCase := func(i int, want bool) func(t *testing.T) {
+		return func(t *testing.T) {
+			res := IsPalindrome(i)
+			require.Equal(t, want, res)
+		}
+
+	}
+
+	t.Run("zero", palidromCase(0, true))
+	t.Run("palindrom", palidromCase(131, true))
+	t.Run("is not palindrom", palidromCase(134, false))
+	t.Run("negative", palidromCase(-423, false))
+	t.Run("negative palidrom", palidromCase(-323, false))
 
 }
