3e0d3561980e4b272a8ff0c44d28b584611aa8d9
Eugenes <smtrkw@mail.ru>
2023-04-27 13:01:14 +0300

    added TestContainsDuplicate

--- internal/pkg/util/util_test.go
+++ internal/pkg/util/util_test.go
@@ -22,3 +22,24 @@ func TestReverseString(t *testing.T) {
 	require.Error(t, err)
 	require.Contains(t, err.Error(), "not int")
 }
+
+func TestContainsDuplicate(t *testing.T) {
+	cases := map[string]struct {
+		slice []int
+		want  bool
+	}{
+		"1": {[]int{1, 2, 1, 3}, true},
+		"2": {[]int{112222, 2, 44, 55}, false},
+		"3": {[]int{}, false},
+		"4": {[]int{2}, false},
+		"5": {[]int{0, 2, -1, 1}, false},
+	}
+
+	for name, testCase := range cases {
+		t.Run(name, func(t *testing.T) {
+			res := ContainsDuplicate(testCase.slice)
+			require.Equal(t, testCase.want, res)
+		})
+	}
+
+}
